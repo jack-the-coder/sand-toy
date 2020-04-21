c35a6333fd1800e8358b16fe300dee16825c2b4b
snazz <jack@snazz.xyz>
2020-04-21 10:50:42 -0500

    remove jiggle

--- player.lua
+++ player.lua
@@ -16,7 +16,7 @@ function updatePlayer(p, getNeighbor, setNeighbor, dt)
     local d = {x = math.random(-1, 1), y = 1}
     local r = {x = math.random(0, 2), y = 0}
     local l = {x = math.random(-2, 0), y = 0}
-    local u = {x = math.random(-1, 1), y = -1}
+    local u = {x = 0, y = -5}
     p.rA = math.random() * 0.2 + 0.7
     if (getNeighbor(d) == 0) then -- gravity
         setNeighbor({x = 0, y = 0}, 0)
@@ -25,24 +25,24 @@ function updatePlayer(p, getNeighbor, setNeighbor, dt)
     	if (getNeighbor(r) == 0 and dDown) then
     		setNeighbor({x = 0, y = 0}, 0)
         	setNeighbor(r, p)
-        	if math.floor(counter) % 25 == 0 then
+        	if math.floor(counter) % 5 == 0 then
         		dDown = false
         	end
         end
         if (getNeighbor(l) == 0 and aDown) then
     		setNeighbor({x = 0, y = 0}, 0)
         	setNeighbor(l, p)
-        	if math.floor(counter) % 25 == 0 then
+        	if math.floor(counter) % 5 == 0 then
         		aDown = false
         	end
         end
-        if (getNeighbor(u) == 0 and wDown) then
-    		setNeighbor({x = 0, y = 0}, 0)
-        	setNeighbor(u, p)
-        	if math.floor(counter) % 25 == 0 then
-        		wDown = false
-        	end
-        end
+        --if (getNeighbor(u) == 0 and wDown) then
+    	--	setNeighbor({x = 0, y = 0}, 0)
+        --	setNeighbor(u, p)
+        --	if math.floor(counter) % 25 == 0 then
+        --		wDown = false
+        --	end
+        --end
     end
     
 end
